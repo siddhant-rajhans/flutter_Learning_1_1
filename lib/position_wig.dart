d5b19a27ca51ebbff11efa54ae6b6cabbe858cc0
Siddhant Rajhans <51508945+siddhant-rajhans@users.noreply.github.com>
2023-10-06 11:06:27 +0530

    position wig added

--- lib/position_wig.dart
+++ lib/position_wig.dart
@@ -3,17 +3,17 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 
 void main(){
-  runApp(position_wig());
+  runApp(const Position_Wig());
 }
 
-class position_wig extends StatefulWidget {
-  const position_wig({super.key});
+class Position_Wig extends StatefulWidget {
+  const Position_Wig({super.key});
 
   @override
-  State<position_wig> createState() => _position_wigState();
+  State<Position_Wig> createState() => _Position_WigState();
 }
 
-class _position_wigState extends State<position_wig> {
+class _Position_WigState extends State<Position_Wig> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -26,23 +26,37 @@ class _position_wigState extends State<position_wig> {
 
           ),
           body:
+          //Position widget can be used with STACK widget not row/column.
+          //reason- stack allows over lapping. Row and columns are fixed in order.
+            Container(
+              width: double.infinity,
+              // width: 300,
+              height: double.infinity,
+              color: Colors.black,
+                child:
+                Stack(
+                  children: [
+                    //Position widget can be used with STACK widget not row/column.
+                    //reason- stack allows over lapping. Row and columns are fixed in order.
+                    //If no other attribute is given the parent will take the child attribute.
+                    Positioned(
+                      bottom: 350,
+                      right: 150,
+                      // top: 10,
+                      // left: 50,
+                      child: Container(
+                        width: 100,
+                        height: 100,
+                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(15), color: Colors.white),
+                        // color: Colors.white,
+                        child: const Center(child: FaIcon(Icons.music_note_rounded, size: 72,color: Colors.red,)),
+                      ),
+                    )
+                  ],
 
-          const Center(child: Center(
-              child:
+                ))
 
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  FaIcon(FontAwesomeIcons.noteSticky, size: 74),
-                  Icon(Icons.music_note_rounded, size: 74, color: Colors.red, )
 
-                ],
-              )
-
-            // Makes Icon
-            // Icon(Icons.music_note_rounded, size: 125, color: Colors.red, )
-          )
-          )
       ),
     );
   }
